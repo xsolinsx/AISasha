64d5ade465fa52732654ece89b1eb5b542f24b0e
xsolinsx <ericsolinas@hotmail.it>
2016-04-30 19:39:18 +0200

    vardump function

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -136,11 +136,97 @@ local function get_dialog_list_callback(extra, success, result)
     end
 end
 
+local function table.show(t, name, indent)
+    local cart
+    -- a container
+    local autoref
+    -- for self references
+
+    --[[ counts the number of elements in a table
+   local function tablecount(t)
+      local n = 0
+      for _, _ in pairs(t) do n = n+1 end
+      return n
+   end
+   ]]
+    -- (RiciLake) returns true if the table is empty
+    local function isemptytable(t) return next(t) == nil end
+
+    local function basicSerialize(o)
+        local so = tostring(o)
+        if type(o) == "function" then
+            local info = debug.getinfo(o, "S")
+            -- info.name is nil because o is not a calling level
+            if info.what == "C" then
+                return string.format("%q", so .. ", C function")
+            else
+                -- the information is defined through lines
+                return string.format("%q", so .. ", defined in (" ..
+                info.linedefined .. "-" .. info.lastlinedefined ..
+                ")" .. info.source)
+            end
+        elseif type(o) == "number" or type(o) == "boolean" then
+            return so
+        else
+            return string.format("%q", so)
+        end
+    end
+
+    local function addtocart(value, name, indent, saved, field)
+        indent = indent or ""
+        saved = saved or { }
+        field = field or name
+
+        cart = cart .. indent .. field
+
+        if type(value) ~= "table" then
+            cart = cart .. " = " .. basicSerialize(value) .. ";\n"
+        else
+            if saved[value] then
+                cart = cart .. " = {}; -- " .. saved[value]
+                .. " (self reference)\n"
+                autoref = autoref .. name .. " = " .. saved[value] .. ";\n"
+            else
+                saved[value] = name
+                -- if tablecount(value) == 0 then
+                if isemptytable(value) then
+                    cart = cart .. " = {};\n"
+                else
+                    cart = cart .. " = {\n"
+                    for k, v in pairs(value) do
+                        k = basicSerialize(k)
+                        local fname = string.format("%s[%s]", name, k)
+                        field = string.format("[%s]", k)
+                        -- three spaces between levels
+                        addtocart(v, fname, indent .. "   ", saved, field)
+                    end
+                    cart = cart .. indent .. "};\n"
+                end
+            end
+        end
+    end
+
+    name = name or "__unnamed__"
+    if type(t) ~= "table" then
+        return name .. " = " .. basicSerialize(t)
+    end
+    cart, autoref = "", ""
+    addtocart(t, name, indent)
+    return cart .. autoref
+end
+
 local function vardump_msg(extra, success, result)
+    local name = 'msg'
+    if extra == 'reply' then
+        name = 'VARDUMP (<reply>)'
+    end
+    if extra == 'msg_id' then
+        name = 'VARDUMP (<msg_id>)'
+    end
     local chat = 'chat#id' .. result.to.peer_id
     local channel = 'channel#id' .. result.to.peer_id
-    send_large_msg(chat, vardump(msg))
-    send_large_msg(channel, vardump(msg))
+    send_large_msg(chat, table.show(result, name))
+    send_large_msg(channel, table.show(result, name))
 end
 
 local function run(msg, matches)
@@ -288,11 +374,11 @@ local function run(msg, matches)
         end
         if matches[1]:lower() == 'vardump' then
             if type(msg.reply_id) ~= "nil" then
-                msgr = get_message(msg.reply_id, vardump_msg, false)
+                msgr = get_message(msg.reply_id, vardump_msg, 'reply')
             elseif matches[2] then
-                msgr = get_message(matches[2], vardump_msg, false)
+                msgr = get_message(matches[2], vardump_msg, 'msg_id')
             else
-                send_large_msg(get_receiver(msg), vardump(msg))
+                send_large_msg(get_receiver(msg), table.show(msg, 'VARDUMP (<msg>)'))
             end
         end
     end
