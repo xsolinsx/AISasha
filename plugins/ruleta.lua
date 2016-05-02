13a5de4899b2278660d9e4a677905bcbba34c5e4
xsolinsx <ericsolinas@hotmail.it>
2016-05-02 15:29:06 +0200

    fix

--- plugins/ruleta.lua
+++ plugins/ruleta.lua
@@ -385,7 +385,7 @@ local function run(msg, matches)
         end
 
         if matches[1]:lower() == 'sfida' then
-            if type(msg.reply_id) ~= "nil" and is_momod(msg) then
+            if type(msg.reply_id) ~= "nil" then
                 get_message(msg.reply_id, Challenge_by_reply, { challenger = user, msg = msg })
             elseif matches[2] then
                 resolve_username(matches[2]:gsub("@", ""), Challenge_by_username, { challenger = user, chat_id = chat, msg = msg })
