a841fa44a0469bb7b42545b280a66b0aac73c4f3
xsolinsx <ericsolinas@hotmail.it>
2016-05-17 19:48:24 +0200

    codify

---
+++ plugins/codify.lua
@@ -0,0 +1,33 @@
+local function callback_reply(extra, success, result)
+    send_large_msg('chat#id' .. result.to.peer_id, "<code>" .. result.text .. "</code>")
+    send_large_msg('channel#id' .. result.to.peer_id, "<code>" .. result.text .. "</code>")
+end
+
+local function run(msg, matches)
+    local receiver = get_receiver(msg)
+    local chat = msg.to.id
+    local chat_type = msg.to.type
+
+    if matches[1]:lower() == 'codify' then
+        if type(msg.reply_id) ~= 'nil' then
+            if is_momod(msg) then
+                return get_message(msg.reply_id, callback_reply, false)
+            else
+                return lang_text('require_mod')
+            end
+        end
+    end
+end
+
+return {
+    description = "CODIFY",
+    patterns =
+    {
+        "^[#!/]([Cc][Oo][Dd][Ii][Ff][Yy])$",
+    },
+    run = run,
+    min_rank = 1
+    -- usage
+    -- MOD
+    -- #codify <reply>
+}
