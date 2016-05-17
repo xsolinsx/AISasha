017ddeb01dc94eeff864ac55f8b8ba27bb14020b
xsolinsx <ericsolinas@hotmail.it>
2016-05-17 20:23:28 +0200

    fix

--- plugins/strings_en.lua
+++ plugins/strings_en.lua
@@ -578,9 +578,10 @@ local function run(msg, matches)
         set_text('broadcast:5', '#broadcast <text>: Sasha sends <text> to all groups.')
 
         -- codify.lua --
-        set_text('codify:0', 2)
+        set_text('codify:0', 3)
         set_text('codify:1', '🅿️ CODIFY')
-        set_text('codify:2', '#codify <text>|<reply>: Sasha codify <text> or the message in <reply>.')
+        set_text('codify:2', 'MOD')
+        set_text('codify:3', '#codify <text>|<reply>: Sasha codify <text> or the message in <reply>.')
 
         -- dogify.lua --
         set_text('dogify:0', 2)
--- plugins/strings_it.lua
+++ plugins/strings_it.lua
@@ -578,9 +578,10 @@ local function run(msg, matches)
         set_text('broadcast:5', '#broadcast <text>: Sasha invia <text> a tutti i gruppi.')
 
         -- codify.lua --
-        set_text('codify:0', 2)
+        set_text('codify:0', 3)
         set_text('codify:1', '🅿️ CODIFY')
-        set_text('codify:2', '#codify <text>|<reply>: Sasha manda <text> o il messaggio specificato in forma di codice.')
+        set_text('codify:2', 'MOD')
+        set_text('codify:3', '#codify <text>|<reply>: Sasha manda <text> o il messaggio specificato in forma di codice.')
 
         -- dogify.lua --
         set_text('dogify:0', 2)
