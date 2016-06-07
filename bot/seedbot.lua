f8521713f33b45e169af59656a0fb0b500284f20
xsolinsx <ericsolinas@hotmail.it>
2016-06-07 15:17:19 +0200

    fix check sudo

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -46,60 +46,60 @@ local function callback_sudo_ids(extra, success, result)
     local tagged = false
     if result.username then
         if extra.msg.text then
-            if string.find(extra.msg.text, '@' .. result.username) or string.find(extra.msg.text, result.print_name:gsub('_', ' ')) then
+            if string.find(extra.msg.text, '@' .. result.username) or string.find(extra.msg.text, result.first_name:gsub('_', ' ')) then
                 tagged = true
             end
         end
         if extra.msg.media then
             if extra.msg.media.title then
-                if string.find(extra.msg.media.title, '@' .. result.username) or string.find(extra.msg.media.title, result.print_name:gsub('_', ' ')) then
+                if string.find(extra.msg.media.title, '@' .. result.username) or string.find(extra.msg.media.title, result.first_name:gsub('_', ' ')) then
                     tagged = true
                 end
             end
             if extra.msg.media.description then
-                if string.find(extra.msg.media.description, '@' .. result.username) or string.find(extra.msg.media.description, result.print_name:gsub('_', ' ')) then
+                if string.find(extra.msg.media.description, '@' .. result.username) or string.find(extra.msg.media.description, result.first_name:gsub('_', ' ')) then
                     tagged = true
                 end
             end
             if extra.msg.media.caption then
-                if string.find(extra.msg.media.caption, '@' .. result.username) or string.find(extra.msg.media.caption, result.print_name:gsub('_', ' ')) then
+                if string.find(extra.msg.media.caption, '@' .. result.username) or string.find(extra.msg.media.caption, result.first_name:gsub('_', ' ')) then
                     tagged = true
                 end
             end
         end
         if extra.msg.fwd_from then
             if extra.msg.fwd_from.title then
-                if string.find(extra.msg.fwd_from.title, '@' .. result.username) or string.find(extra.msg.fwd_from.title, result.print_name:gsub('_', ' ')) then
+                if string.find(extra.msg.fwd_from.title, '@' .. result.username) or string.find(extra.msg.fwd_from.title, result.first_name:gsub('_', ' ')) then
                     tagged = true
                 end
             end
         end
     else
         if extra.msg.text then
-            if string.find(extra.msg.text, result.print_name:gsub('_', ' ')) then
+            if string.find(extra.msg.text, result.first_name:gsub('_', ' ')) then
                 tagged = true
             end
         end
         if extra.msg.media then
             if extra.msg.media.title then
-                if string.find(extra.msg.media.title, result.print_name:gsub('_', ' ')) then
+                if string.find(extra.msg.media.title, result.first_name:gsub('_', ' ')) then
                     tagged = true
                 end
             end
             if extra.msg.media.description then
-                if string.find(extra.msg.media.description, result.print_name:gsub('_', ' ')) then
+                if string.find(extra.msg.media.description, result.first_name:gsub('_', ' ')) then
                     tagged = true
                 end
             end
             if extra.msg.media.caption then
-                if string.find(extra.msg.media.caption, result.print_name:gsub('_', ' ')) then
+                if string.find(extra.msg.media.caption, result.first_name:gsub('_', ' ')) then
                     tagged = true
                 end
             end
         end
         if extra.msg.fwd_from then
             if extra.msg.fwd_from.title then
-                if string.find(extra.msg.fwd_from.title, result.print_name:gsub('_', ' ')) then
+                if string.find(extra.msg.fwd_from.title, result.first_name:gsub('_', ' ')) then
                     tagged = true
                 end
             end
