de3c81d16b02a2551d007d3d1d9cdf141ad3b393
xsolinsx <ericsolinas@hotmail.it>
2016-06-13 19:29:01 +0200

    fix feedback

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,18 +1,18 @@
 ﻿function run(msg, matches)
-    local text = lang_text(receiver, 'feedStart')
+    local text = lang_text('feedStart')
     if msg.from.first_name then
-        text = text .. lang_text(receiver, 'feedName') .. msg.from.first_name
+        text = text .. lang_text('feedName') .. msg.from.first_name
     end
     if msg.from.last_name then
-        text = text .. lang_text(receiver, 'feedSurname') .. msg.from.last_name
+        text = text .. lang_text('feedSurname') .. msg.from.last_name
     end
     if msg.from.username then
-        text = text .. lang_text(receiver, 'feedUsername') .. msg.from.username
+        text = text .. lang_text('feedUsername') .. msg.from.username
     end
     text = text .. '\n🆔: ' .. msg.from.id ..
     '\n\nFeedback:\n' .. matches[1]
     send_large_msg('chat#id120307338', text)
-    return lang_text(receiver, 'feedSent')
+    return lang_text('feedSent')
 end
 
 return {
