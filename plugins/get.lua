fe6075f7bbc971ba6c11db46fc8573bf15c8c5dc
xsolinsx <ericsolinas@hotmail.it>
2016-06-25 00:23:27 +0200

    fix get pre_process

--- plugins/get.lua
+++ plugins/get.lua
@@ -43,27 +43,29 @@ local function run(msg, matches)
 end
 
 local function pre_process(msg, matches)
-    local vars = list_variables(msg)
+    if msg.text then
+        local vars = list_variables(msg)
 
-    if vars ~= nil then
-        local t = vars:split('\n')
-        for i, word in pairs(t) do
-            local temp = word:lower()
-            if word:lower() ~= 'get' and string.find(msg.text:lower(), temp) then
-                local value = get_value(msg, word:lower())
-                if value then
-                    if not string.match(value, "^(.*)user%.(%d+)%.variables(.*)$") and not string.match(value, "^(.*)chat%.(%d+)%.variables(.*)$") and not string.match(value, "^(.*)channel%.(%d+)%.variables(.*)$") then
-                        -- if not media
-                        reply_msg(msg.id, get_value(msg, word:lower()), ok_cb, false)
-                    elseif string.match(value, "^(.*)user%.(%d+)%.variables(.*)%.jpg$") or string.match(value, "^(.*)chat%.(%d+)%.variables(.*)%.jpg$") or string.match(value, "^(.*)channel%.(%d+)%.variables(.*)%.jpg$") then
-                        -- if picture
-                        if io.popen('find ' .. value):read("*all") ~= '' then
-                            send_photo(get_receiver(msg), value, ok_cb, false)
-                        end
-                    elseif string.match(value, "^(.*)user%.(%d+)%.variables(.*)%.ogg$") or string.match(value, "^(.*)chat%.(%d+)%.variables(.*)%.ogg$") or string.match(value, "^(.*)channel%.(%d+)%.variables(.*)%.ogg$") then
-                        -- if audio
-                        if io.popen('find ' .. value):read("*all") ~= '' then
-                            send_audio(get_receiver(msg), value, ok_cb, false)
+        if vars ~= nil then
+            local t = vars:split('\n')
+            for i, word in pairs(t) do
+                local temp = word:lower()
+                if word:lower() ~= 'get' and string.find(msg.text:lower(), temp) then
+                    local value = get_value(msg, word:lower())
+                    if value then
+                        if not string.match(value, "^(.*)user%.(%d+)%.variables(.*)$") and not string.match(value, "^(.*)chat%.(%d+)%.variables(.*)$") and not string.match(value, "^(.*)channel%.(%d+)%.variables(.*)$") then
+                            -- if not media
+                            reply_msg(msg.id, get_value(msg, word:lower()), ok_cb, false)
+                        elseif string.match(value, "^(.*)user%.(%d+)%.variables(.*)%.jpg$") or string.match(value, "^(.*)chat%.(%d+)%.variables(.*)%.jpg$") or string.match(value, "^(.*)channel%.(%d+)%.variables(.*)%.jpg$") then
+                            -- if picture
+                            if io.popen('find ' .. value):read("*all") ~= '' then
+                                send_photo(get_receiver(msg), value, ok_cb, false)
+                            end
+                        elseif string.match(value, "^(.*)user%.(%d+)%.variables(.*)%.ogg$") or string.match(value, "^(.*)chat%.(%d+)%.variables(.*)%.ogg$") or string.match(value, "^(.*)channel%.(%d+)%.variables(.*)%.ogg$") then
+                            -- if audio
+                            if io.popen('find ' .. value):read("*all") ~= '' then
+                                send_audio(get_receiver(msg), value, ok_cb, false)
+                            end
                         end
                     end
                 end
