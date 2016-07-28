3ddbac8d480664b88fcdb50489a0883038491a40
xsolinsx <ericsolinas@hotmail.it>
2016-07-28 19:55:30 +0200

    encode pretty json

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -471,7 +471,7 @@ function load_data(filename)
 end
 
 function save_data(filename, data)
-    local s = JSON.encode(data)
+    local s = JSON.encode_pretty(data)
     local f = io.open(filename, 'w')
     f:write(s)
     f:close()
