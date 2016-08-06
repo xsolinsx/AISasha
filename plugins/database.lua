ea2a33bab207a30399f96647686f355be6e4939d
xsolinsx <ericsolinas@hotmail.it>
2016-08-06 12:39:08 +0200

    pre_process function and removed functions to get all groups and supergroups

--- plugins/database.lua
+++ plugins/database.lua
@@ -1,89 +1,3 @@
-local function callback_all_supergroups_members(extra, success, result)
-    local database = extra.database
-    local chat_id = string.match(extra.receiver, '%d+')
-    print('supergroups members ' .. chat_id)
-
-    -- save users info
-    for k, v in pairsByKeys(result) do
-        if v.print_name then
-            if database["users"][tostring(v.peer_id)] then
-                print('already registered user')
-                if database["users"][tostring(v.peer_id)]['groups'] then
-                    if not database["users"][tostring(v.peer_id)]['groups'][tostring(chat_id)] then
-                        database["users"][tostring(v.peer_id)]['groups'][tostring(chat_id)] = tonumber(chat_id)
-                    end
-                else
-                    database["users"][tostring(v.peer_id)]['groups'] = { [tostring(chat_id)] = tonumber(chat_id) }
-                end
-                if database["users"][tostring(v.peer_id)]['print_name'] ~= v.print_name:gsub("_", " ") then
-                    database["users"][tostring(v.peer_id)]['print_name'] = v.print_name:gsub("_", " ")
-                    database["users"][tostring(v.peer_id)]['old_print_names'] = database["users"][tostring(v.peer_id)]['old_print_names'] .. ' ### ' .. v.print_name:gsub("_", " ")
-                end
-                local username = 'NOUSER'
-                if v.username then
-                    username = '@' .. v.username
-                end
-                if database["users"][tostring(v.peer_id)]['username'] ~= username then
-                    database["users"][tostring(v.peer_id)]['username'] = username
-                    database["users"][tostring(v.peer_id)]['old_usernames'] = database["users"][tostring(v.peer_id)]['old_usernames'] .. ' ### ' .. username
-                end
-            else
-                print('new user')
-                local username = 'NOUSER'
-                if v.username then
-                    username = '@' .. v.username
-                end
-                database["users"][tostring(v.peer_id)] = {
-                    print_name = v.print_name:gsub("_"," "),
-                    old_print_names = v.print_name:gsub("_"," "),
-                    username = username,
-                    old_usernames = username,
-                    long_id = v.id,
-                    groups = { [tostring(chat_id)] = tonumber(chat_id) },
-                }
-            end
-        end
-    end
-    save_data(_config.database.db, database)
-end
-
-local function callback_all_supergroups_info(extra, success, result)
-    local database = extra.database
-    local chat_id = result.peer_id
-    print('supergroups info ' .. chat_id)
-
-    -- save supergroup info
-    if database["groups"][tostring(chat_id)] then
-        print('already registered group')
-        if database["groups"][tostring(chat_id)]['print_name'] ~= result.print_name:gsub("_", " ") then
-            database["groups"][tostring(chat_id)]['print_name'] = result.print_name:gsub("_", " ")
-            database["groups"][tostring(chat_id)]['old_print_names'] = database["groups"][tostring(chat_id)]['old_print_names'] .. ' ### ' .. result.print_name:gsub("_", " ")
-        end
-        local username = 'NOUSER'
-        if result.username then
-            username = '@' .. result.username
-        end
-        if database["groups"][tostring(chat_id)]['username'] ~= username then
-            database["groups"][tostring(chat_id)]['username'] = username
-            database["groups"][tostring(chat_id)]['old_usernames'] = database["groups"][tostring(chat_id)]['old_usernames'] .. ' ### ' .. username
-        end
-    else
-        print('new group')
-        local username = 'NOUSER'
-        if result.username then
-            username = '@' .. result.username
-        end
-        database["groups"][tostring(chat_id)] = {
-            print_name = result.print_name:gsub("_"," "),
-            old_print_names = result.print_name:gsub("_"," "),
-            username = username,
-            old_usernames = username,
-            lang = get_lang(result.peer_id),
-            long_id = result.id,
-        }
-    end
-end
-
 local function callback_group_database(extra, success, result)
     local database = extra.database
     local chat_id = result.peer_id
@@ -234,6 +148,92 @@ local function callback_supergroup_database(extra, success, result)
     send_large_msg(extra.receiver, langs[get_lang(string.match(extra.receiver, '%d+'))].dataLeaked)
 end
 
+local function pre_process(msg)
+    local database = load_data(_config.database.db)
+    -- save user info
+    if msg.action then
+        if msg.action.user then
+            if msg.action.user.print_name then
+                if database["users"][tostring(msg.action.user.id)] then
+                    print('already registered user action')
+                    if database["users"][tostring(msg.action.user.id)]['groups'] then
+                        if not database["users"][tostring(msg.action.user.id)]['groups'][tostring(msg.to.id)] then
+                            database["users"][tostring(msg.action.user.id)]['groups'][tostring(msg.to.id)] = tonumber(msg.to.id)
+                        end
+                    else
+                        database["users"][tostring(msg.action.user.id)]['groups'] = { [tostring(msg.to.id)] = tonumber(msg.to.id) }
+                    end
+                    if database["users"][tostring(msg.action.user.id)]['print_name'] ~= msg.action.user.print_name:gsub("_", " ") then
+                        database["users"][tostring(msg.action.user.id)]['print_name'] = msg.action.user.print_name:gsub("_", " ")
+                        database["users"][tostring(msg.action.user.id)]['old_print_names'] = database["users"][tostring(msg.action.user.id)]['old_print_names'] .. ' ### ' .. msg.action.user.print_name:gsub("_", " ")
+                    end
+                    local username = 'NOUSER'
+                    if msg.action.user.username then
+                        username = '@' .. msg.action.user.username
+                    end
+                    if database["users"][tostring(msg.action.user.id)]['username'] ~= username then
+                        database["users"][tostring(msg.action.user.id)]['username'] = username
+                        database["users"][tostring(msg.action.user.id)]['old_usernames'] = database["users"][tostring(msg.action.user.id)]['old_usernames'] .. ' ### ' .. username
+                    end
+                else
+                    print('new user action')
+                    local username = 'NOUSER'
+                    if msg.action.user.username then
+                        username = '@' .. msg.action.user.username
+                    end
+                    database["users"][tostring(msg.action.user.id)] = {
+                        print_name = msg.action.user.print_name:gsub("_"," "),
+                        old_print_names = msg.action.user.print_name:gsub("_"," "),
+                        username = username,
+                        old_usernames = username,
+                        long_id = msg.action.user.peer_id,
+                        groups = { [tostring(msg.to.id)] = tonumber(msg.to.id) },
+                    }
+                end
+            end
+        end
+    end
+    if msg.from.print_name then
+        if database["users"][tostring(msg.from.id)] then
+            print('already registered user')
+            if database["users"][tostring(msg.from.id)]['groups'] then
+                if not database["users"][tostring(msg.from.id)]['groups'][tostring(msg.to.id)] then
+                    database["users"][tostring(msg.from.id)]['groups'][tostring(msg.to.id)] = tonumber(msg.to.id)
+                end
+            else
+                database["users"][tostring(msg.from.id)]['groups'] = { [tostring(msg.to.id)] = tonumber(msg.to.id) }
+            end
+            if database["users"][tostring(msg.from.id)]['print_name'] ~= msg.from.print_name:gsub("_", " ") then
+                database["users"][tostring(msg.from.id)]['print_name'] = msg.from.print_name:gsub("_", " ")
+                database["users"][tostring(msg.from.id)]['old_print_names'] = database["users"][tostring(msg.from.id)]['old_print_names'] .. ' ### ' .. msg.from.print_name:gsub("_", " ")
+            end
+            local username = 'NOUSER'
+            if msg.from.username then
+                username = '@' .. msg.from.username
+            end
+            if database["users"][tostring(msg.from.id)]['username'] ~= username then
+                database["users"][tostring(msg.from.id)]['username'] = username
+                database["users"][tostring(msg.from.id)]['old_usernames'] = database["users"][tostring(msg.from.id)]['old_usernames'] .. ' ### ' .. username
+            end
+        else
+            print('new user')
+            local username = 'NOUSER'
+            if msg.from.username then
+                username = '@' .. msg.from.username
+            end
+            database["users"][tostring(msg.from.id)] = {
+                print_name = msg.from.print_name:gsub("_"," "),
+                old_print_names = msg.from.print_name:gsub("_"," "),
+                username = username,
+                old_usernames = username,
+                long_id = msg.from.peer_id,
+                groups = { [tostring(msg.to.id)] = tonumber(msg.to.id) },
+            }
+        end
+    end
+    save_data(_config.database.db, database)
+end
+
 local function run(msg, matches)
     if is_sudo(msg) then
         if matches[1]:lower() == 'createdatabase' then
@@ -244,7 +244,7 @@ local function run(msg, matches)
             return
         end
 
-        if matches[1]:lower() == 'dogroupdatabase' or matches[1]:lower() == 'sasha esegui database gruppo' then
+        if matches[1]:lower() == 'dodatabase' or matches[1]:lower() == 'sasha esegui database' then
             local receiver = get_receiver(msg)
             local database = load_data(_config.database.db)
             if msg.to.type == 'channel' then
@@ -254,35 +254,6 @@ local function run(msg, matches)
             else
                 return
             end
-        end
-
-        if matches[1]:lower() == 'dodatabase' or matches[1]:lower() == 'sasha esegui database' then
-            local data = load_data(_config.moderation.data)
-            if data['groups'] then
-                local i = 1
-                for k, v in pairsByKeys(data['groups']) do
-                    local function post_get_db_groups()
-                        local database = load_data(_config.database.db)
-                        channel_get_users('channel#id' .. tostring(v), callback_all_supergroups_members, { receiver = 'channel#id' .. tostring(v), database = database })
-                        channel_info('channel#id' .. tostring(v), callback_all_supergroups_info, { receiver = 'channel#id' .. tostring(v), database = database })
-                        chat_info('chat#id' .. tostring(v), callback_group_database, { receiver = 'chat#id' .. tostring(v), database = database })
-                    end
-                    postpone(post_get_db_groups, false, i)
-                    i = i + 5
-                end
-            end
-
-            if data['realms'] then
-                for k, v in pairsByKeys(data['realms']) do
-                    local function post_get_db_groups()
-                        local database = load_data(_config.database.db)
-                        chat_info('chat#id' .. tostring(v), callback_all_groups, { receiver = 'chat#id' .. tostring(v), database = database })
-                    end
-                    postpone(post_get_db_groups, false, i)
-                    i = i + 5
-                end
-            end
-            return langs[msg.lang].dataLeaked
         end
 
         if (matches[1]:lower() == 'search' or matches[1]:lower() == 'sasha cerca' or matches[1]:lower() == 'cerca') and matches[2] then
@@ -379,13 +350,10 @@ return {
     patterns =
     {
         "^[#!/]([Cc][Rr][Ee][Aa][Tt][Ee][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee])$",
-        "^[#!/]([Dd][Oo][Gg][Rr][Oo][Uu][Pp][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee])$",
         "^[#!/]([Dd][Oo][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee])$",
         "^[#!/]([Ss][Ee][Aa][Rr][Cc][Hh]) (%d+)$",
         "^[#!/]([Aa][Dd][Dd][Rr][Ee][Cc][Oo][Rr][Dd]) ([^%s]+) (.*)$",
         "^[#!/]([Dd][Ee][Ll][Ee][Tt][Ee]) (%d+)$",
-        -- dogroupdatabase
-        "^([Ss][Aa][Ss][Hh][Aa] [Ee][Ss][Ee][Gg][Uu][Ii] [Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee] [Gg][Rr][Uu][Pp][Pp][Oo])$",
         -- dodatabase
         "^([Ss][Aa][Ss][Hh][Aa] [Ee][Ss][Ee][Gg][Uu][Ii] [Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee])$",
         -- search
@@ -396,11 +364,11 @@ return {
         "^([Ee][Ll][Ii][Mm][Ii][Nn][Aa]) (%d+)$",
     },
     run = run,
+    pre_process = pre_process,
     min_rank = 5
     -- usage
     -- SUDO
     -- #createdatabase
-    -- (#dogroupdatabase|sasha esegui database gruppo)
     -- (#dodatabase|sasha esegui database)
     -- (#search|[sasha] cerca) <id>
     -- (#delete|[sasha] elimina) <id>
