4ed46a74bd308c6c8835d4271ecc94dd9eefa09a
xsolinsx <ericsolinas@hotmail.it>
2016-08-29 13:05:34 +0200

    fix

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -1,23 +1,31 @@
 ﻿local function run(msg, matches)
-    if matches[1] == 'br' and is_admin1(msg) then
-        local response = matches[3]
-        send_large_msg("chat#id" .. matches[2], response)
-        send_large_msg("channel#id" .. matches[2], response)
+    if matches[1] == 'br' then
+        if is_admin1(msg) then
+            local response = matches[3]
+            send_large_msg("chat#id" .. matches[2], response)
+            send_large_msg("channel#id" .. matches[2], response)
+        else
+            return langs[msg.lang].require_admin
+        end
     end
-    if matches[1] == 'broadcast' and is_sudo(msg) then
-        -- Only sudo!
-        local data = load_data(_config.moderation.data)
-        local groups = 'groups'
-        local response = matches[2]
-        for k, v in pairs(data[tostring(groups)]) do
-            chat_id = v
-            local chat = 'chat#id' .. chat_id
-            local channel = 'channel#id' .. chat_id
-            local function post_msg()
-                send_large_msg(chat, response)
-                send_large_msg(channel, response)
+    if matches[1] == 'broadcast' then
+        if is_sudo(msg) then
+            -- Only sudo!
+            local data = load_data(_config.moderation.data)
+            local groups = 'groups'
+            local response = matches[2]
+            for k, v in pairs(data[tostring(groups)]) do
+                chat_id = v
+                local chat = 'chat#id' .. chat_id
+                local channel = 'channel#id' .. chat_id
+                local function post_msg()
+                    send_large_msg(chat, response)
+                    send_large_msg(channel, response)
+                end
+                postpone(post_msg, false, math.fmod(math.random(1, 1000), 30) + 1)
             end
-            postpone(post_msg, false, math.fmod(math.random(1, 1000), 30) + 1)
+        else
+            return langs[msg.lang].require_sudo
         end
     end
 end
