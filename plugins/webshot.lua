cee3e8175dabd3e66ed5ac6c9e6022157bf7a3f0
xsolinsx <ericsolinas@hotmail.it>
2017-12-24 20:47:37 +0100

    fix

--- plugins/webshot.lua
+++ plugins/webshot.lua
@@ -1,12 +1,12 @@
 ﻿local function run(msg, matches)
-    if msg.from.is_mod then
-        local url = "http://webshot.okfnlabs.org/api/generate?url=" .. html_escape("http%3A%2F%2F" .. matches[1])
+    if is_momod(msg) then
+        local url = "http://webshot.okfnlabs.org/api/generate?url=" .. URL.escape(matches[1])
         if matches[2] and not matches[3] then
             url = url .. "&full=true"
         elseif matches[2] and matches[3] then
             url = url .. "&width=" .. matches[2] .. "&height=" .. matches[3]
         end
-        return sendPhotoFromUrl(msg.chat.id, url)
+        return send_photo_from_url(get_receiver(msg), url)
     else
         return langs[msg.lang].require_mod
     end
