493e5f6924498794c0536758f7ca845dbeb05203
xsolinsx <ericsolinas@hotmail.it>
2018-08-10 17:29:19 +0200

    fix for installation

--- launch.sh
+++ launch.sh
@@ -84,7 +84,7 @@ install() {
   if [ $RET -ne 0 ]; then
     autoconf -i
   fi
-  ./configure && make
+  ./configure --disable-openssl && make
 
   RET=$?; if [ $RET -ne 0 ]; then
     echo "Error. Exiting."; exit $RET;
